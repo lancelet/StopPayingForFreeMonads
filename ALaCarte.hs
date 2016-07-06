ff103db8985614348903b0dbaa2b81329f49aee8
Jonathan Merritt <j.s.merritt@gmail.com>
2016-07-06 16:14:10 +1000

    WIP

--- ALaCarte.hs
+++ ALaCarte.hs
@@ -20,7 +20,8 @@ data (f :+: g) a = Inl (f a) | Inr (g a) deriving (Show, Eq)
 
 
 instance (Functor f, Functor g) => Functor (f :+: g) where
-  fmap = undefined
+  fmap g (Inl x) = Inl (fmap g x)
+  fmap g (Inr x) = Inr (fmap g x)
 
 class (Functor f, Functor g) => f :<: g where
   inj :: f a -> g a
@@ -29,19 +30,22 @@ class (Functor f, Functor g) => f :<: g where
 
 -- Injections
 instance {-# OVERLAPS #-} (Functor f) => f :<: f where
-  inj = undefined
-  prj = undefined
+  inj = id
+  prj = Just . id
 
 instance {-# OVERLAPS #-} (Functor f, Functor g) => f :<: (f :+: g) where
-  inj = undefined
-  prj = undefined
+  inj f = Inl f
+  prj (Inl f) = Just f
+  prj (Inr _) = Nothing
 
 instance {-# OVERLAPS #-} (Functor f, Functor g, Functor h, f :<: h) => f :<: (g :+: h) where
-  inj = undefined
-  prj = undefined
+  inj f = Inr (inj f)
+  prj (Inl _) = Nothing
+  prj (Inr h) = prj h
 
 inject :: (f :<: g) => f (Free g a) -> Free g a
-inject = undefined
+inject = Free . inj
+
 
 -- |Free monad
 data Free f a = Pure a | Free (f (Free f a)) deriving Functor
@@ -50,14 +54,17 @@ deriving instance (Show a, Show (f (Free f a))) => Show (Free f a)
 deriving instance (Eq a, Eq (f (Free f a))) => Eq (Free f a)
 
 instance Functor f => Applicative (Free f) where
-  pure  = undefined
-  (<*>) = undefined
+  pure = Pure
+  Pure g <*> a = fmap g a
+  Free g <*> a = Free (fmap (<*> a) g)
 
 instance Functor f => Monad (Free f) where
-  (>>=) = undefined
+  Pure x >>= g = g x
+  Free x >>= g = Free (fmap (>>= g) x)
 
 foldFree :: Functor f => (a -> b) -> (f b -> b) -> Free f a -> b
-foldFree = undefined
+foldFree g _ (Pure x) = g x
+foldFree g e (Free x) = e (fmap (foldFree g e) x)
 
 -- Interpreting, using a type class to compose interpretations
 
@@ -65,7 +72,8 @@ class (Functor f, Monad m) => Interpret f m where
   intp :: f a -> m a
 
 instance (Interpret f m, Interpret g m) => Interpret (f :+: g) m where
-  intp = undefined
+  intp (Inl x) = intp x
+  intp (Inr x) = intp x
 
 interpret :: (Interpret f m) => Free f a -> m a
 interpret = undefined
